491f61e394a07af314c0bfe5ede6d1621b3b54b9
Keelan Chu For <kkchufor@gmail.com>
2014-12-09 13:52:13 -0500

    Revert "Adds colormap parameter"
    This reverts commit af10260986b90dbf2af00da6f6db275dd11c7ec1.

--- plots2subplots.m
+++ plots2subplots.m
@@ -1,29 +1,30 @@
-function hFig = plots2subplots(haxes, varargin)
+function plots2subplots(haxes)
 %PLOTS2SUBPLOTS Copy multiple figures to a single subplot-formatted figure.
-%   PLOTS2SUBPLOTS() creates a single subplot-formatted figure from copies
-%   of all open figures.
-%
-%   PLOTS2SUBPLOTS('all') has the same effect as PLOTS2SUBPLOTS().
-%
 %   PLOTS2SUBPLOTS(haxes) creates a single subplot-formatted figure from
 %   the axes handles specified in haxes.
 %
-%   PLOTS2SUBPLOTS(...,'Colormap',MAP) sets the subplots' colormaps to MAP.
-%   Type HELP GRAPH3D to see a number of useful colormaps.
+%   PLOTS2SUBPLOTS() creates a single subplot-formatted figure from copies
+%   of all open figures.
 %
 %   hFig = PLOTS2SUBPLOTS(...) retrieves the subplot figure handle.
 %
 %   The order of subplots is made as square as possible (width >= height).
 %
-%   See also PRETTYPLOTS, COLORMAP.
+%   See also PRETTYPLOTS.
 %   https://github.com/keelanc/
 
 %   Author: Keelan Chu For
-%   2014-12-09
+%   2014-09-03
 %   https://github.com/keelanc/plots2subplots
 
+% test
+% close all
+% for ii=1:14
+%     figure
+%     ezplot(['x^',num2str(ii)])
+% end
 
-if nargin == 0 || strcmp(haxes,'all') % find all open figures
+if nargin == 0          % find all open figures if none are specified
     haxes = findobj('type','axes');
     haxes = haxes(end:-1:1);                      % plot oldest first
 end
@@ -44,16 +45,4 @@ for ii=1:ll
     set(haxesnew,'Position',newPos);            % and adjust position
     set(get(haxesnew,'parent'), 'Colormap',...  % and maintain original
         get(get(haxes(ii),'parent'), 'Colormap')); % colormap
-    
-    if nargin > 1
-        if any(strcmp(varargin, 'Colormap'))
-            ind = find(strcmp(varargin, 'Colormap'));
-            try
-                set(get(haxesnew,'parent'), 'Colormap',...
-                colormap(varargin{ind+1}));
-            catch
-                error('Invalid colormap');
-            end
-        end
-    end
 end
